b066f353fd2f5c7f986ecf46a7516f2cdf257be9
Leo McClellan <mcclellan2@usf.edu>
2024-10-04 18:16:39 -0400

    Create Employee class with properties and methods

--- employee_management.js
+++ employee_management.js
@@ -1,1 +1,14 @@
 // Employee Management System
+// Task 1: Create an employee class
+class Employee {
+    constructor(name, salary, position, department) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+    };
+    // return a string of name, position, and salary
+    getDetails() {
+        console.log(`${this.name} works as a ${position} and has a salary of $${salary}`);
+    }
+};
