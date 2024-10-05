fdce8e97cd7530df3bd354799a015e71f76ddbee
Leo McClellan <mcclellan2@usf.edu>
2024-10-05 17:11:58 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -1,14 +1,29 @@
 // Employee Management System
 // Task 1: Create an employee class
 class Employee {
-    constructor(name, salary, position, department) {
-        this.name = name;
-        this.salary = salary;
-        this.position = position;
-        this.department = department;
-    };
-    // return a string of name, position, and salary
-    getDetails() {
-        console.log(`${this.name} works as a ${position} and has a salary of $${salary}`);
-    }
+  constructor(name, salary, position, department) {
+    this.name = name;
+    this.salary = salary;
+    this.position = position;
+    this.department = department;
+  }
+  // return a string of name, position, and salary
+  getDetails() {
+    return `${this.name} works as a ${this.position} and has a salary of $${this.salary}.`;
+  }
+}
+
+// Task 2: Create a department class
+class Department {
+  constructor(name, employees) {
+    this.name = name;
+    this.employees = employees;
+  };
+  addEmployee(employee) {
+    this.employees.push(employee);
+  };
+  getDepartmentSalary() {
+    const total = this.employees.reduce((sum, employee) => sum + employee.salary, 0);
+    return total;
+  };
 };
