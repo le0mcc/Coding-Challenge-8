d601a0d2e0b524904cbd678f680c0f3b1e498fa1
Leo McClellan <mcclellan2@usf.edu>
2024-10-05 17:15:27 -0400

    Create Manager class with inheritance from Employee

--- employee_management.js
+++ employee_management.js
@@ -27,3 +27,25 @@ class Department {
     return total;
   };
 };
+
+const employee = new Employee("Ella", 60000, "Supervisor", "Sales");
+const department = new Department("Sales", [employee]);
+const employeeTwo = new Employee("Derek", 40000, "Employee", "Sales");
+department.addEmployee(employeeTwo);
+console.log(department);
+console.log(department.getDepartmentSalary());
+
+//Task 3: Create a manager class that inherits from employee
+class Manager extends Employee {
+    constructor(name, salary, position, department, bonus) {
+        super(name, salary, position, department);
+        this.bonus = bonus;
+    };
+    getDetails() {
+        return `${this.name} works as a ${this.position} and has a salary of $${this.salary} with a bonus of $${this.bonus}.`;
+    };
+};
+
+// Task 4: Handle bonuses for managers
+
+// Task 5: Create and manage departments and employees
